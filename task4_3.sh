a00f0e07930b77f458b30e053183f964fc0ef3e4
alex bilyk <asbilyk@gmail.com>
2018-04-01 23:54:39 +0300

    backup

--- task4_3.sh
+++ task4_3.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
-if [[$# -ne 2]]
+if ["$@" != 0 ]
 then
-echo "Wrong parameters"
-exit
+  exit
+  echo "Wrong parameters"
 fi
-if [[-n $1]]
+if [-z "$1"]
 then
-echo "Folder doesn\'t exist"
-exit
+  echo "Folder doesn\'t exist"
 fi
-DIR=$1
+DIR="$1"
 NOW=$(date +"%d-%m-%Y")
-NAME=$(sed -e 's/\//; s/\//-/' DIR)
+NAME=$(sed -e 's/\//; s/\//-/' $DIR)
 BACKUP='/tmp/backups/'
+if [-d $BACKUP]; then
+  echo 'folder exist'>/dev/null
+else
+  mkdir $BACKUP
+fi
 if [$2 -lt $(ls $BACKUP/$NAME*|wc -l)]
 then
-#$NAME|xargs rm -rf
+  $NAME|xargs rm -rf
 fi
+tar -czvf $BACKUP/$NAME.tar.gz $DIR  
  
